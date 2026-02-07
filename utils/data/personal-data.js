ab82717ae2333960c9c074e5ba733cd5990c9a2b
Muddasir <muhammadmuddasir374@gmail.com>
2026-02-07 12:08:57 +0500

    profile and projects img updated

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -1,6 +1,6 @@
 export const personalData = {
   name: "Muhammad Muddasir",
-  profile: '/profile.png',
+  profile: '/profile.jpg',
   designation: "Software Developer",
   description: "My name is Muhammad Muddasir. I am a software developer with hands on experience in Flutter and Dart, building scalable mobile applications backed by both relational and non-relational databases. I work effectively in both team-based and independent environments and can deliver under pressure. I continuously improve my skills through self-directed learning and by tackling real-world development challenges. ",
   email: 'muhammadmuddasir.dev@gmail.com',
